f1032a2d3e6c64dac2d34578c5b4765a57ac1760
Mahdup <mahdup@test.com>
2025-06-08 23:38:03 +0330

    add dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Login from "./Page/Login/login";
+import Home from "./Page/Home/home";
+import "./styles/reset.css";
+import "./styles/fonts.css";
 import "./styles/App.css";
 
 const App = () => {
@@ -9,6 +11,7 @@ const App = () => {
       <main>
         <Routes>
           <Route path="/" element=<Login /> />
+          <Route path="/dashboard" element=<Home /> />
         </Routes>
       </main>
     </Router>
